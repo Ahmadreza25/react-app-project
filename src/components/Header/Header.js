42dbfe4a1edc367d527dff66e59cf25a559eefcb
ahmadreza25 <ywsfwnda8@gmail.com>
2024-10-06 16:42:01 +0330

    add code page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,26 @@
 import React from "react";
+import logo from "../../images/img/logo.png"
+import "./Header.css"
 
 
 const Header = () => {
     return(
         <div>
-            
+            <div className="div-main-header">
+                <header className="header">
+                    <div>
+                        <img src={logo} className="logo" alt="logo"/>
+                    </div>
+                    <div className="">
+
+                    </div>
+                    <div className="">
+
+                    </div>
+                </header>
+            </div>
         </div>
     )
 }
+
+export default Header

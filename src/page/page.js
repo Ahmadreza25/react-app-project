5eefb5bced47c0d799a0b47f1e273f6e30304b63
ahmadreza25 <ywsfwnda8@gmail.com>
2024-10-08 14:52:37 +0330

    add code banner

--- src/page/page.js
+++ src/page/page.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Header from "../components/Header/Header";
+import Banner from "../components/Banner/Banner";
 
 const Page = () => {
     return(
         <div>
-            <div>
+            <div className="header-main">
                 <Header />
+            </div>
+            <div>
+                <Banner />
             </div>
         </div>
     )

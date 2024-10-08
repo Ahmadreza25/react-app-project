df4e25d730180ee9a8457a21d832fb866e38c6c8
ahmadreza25 <ywsfwnda8@gmail.com>
2024-10-08 21:18:43 +0330

    add code slider

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,17 +5,19 @@ import "./Banner.css"
 const Banner = () => {
     return(
         <div>
-            <div className="div-main-banner">
-                <div className="div-banner">
-                    <div className="banner">
-                        <h1 className="h1-banner">
-                            FIND YOUR LOCAL CAMPOS CAFE
-                        </h1>
-                    </div>
-                    <div className="div-btn-banner">
-                        <button className="btn-1">
-                            CAFE FINDER
-                        </button>
+            <div className="main-banner">
+                <div className="div-main-banner">
+                    <div className="div-banner">
+                        <div className="banner">
+                            <h1 className="h1-banner">
+                                FIND YOUR LOCAL CAMPOS CAFE
+                            </h1>
+                        </div>
+                        <div className="div-btn-banner">
+                            <button className="btn-1">
+                                CAFE FINDER
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/page/page.js
+++ src/page/page.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Header/Header";
 import Banner from "../components/Banner/Banner";
-
+import Styler from "../components/Styler/Styler";
 const Page = () => {
     return(
         <div>
@@ -10,6 +10,9 @@ const Page = () => {
             </div>
             <div>
                 <Banner />
+            </div>
+            <div>
+                <Styler />
             </div>
         </div>
     )

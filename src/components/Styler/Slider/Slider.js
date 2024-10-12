1f17b535ff278a1a22e9df72b1f69053e7cffa77
ahmadreza25 <ywsfwnda8@gmail.com>
2024-10-12 21:01:39 +0330

    add code slider

--- src/components/Styler/Slider/Slider.js
+++ src/components/Styler/Slider/Slider.js
@@ -1,3 +1,4 @@
+import React from "react"
 import img1 from "../../../images/img-cart/img1.png"
 import img2 from "../../../images/img-cart/img2.png"
 import img3 from "../../../images/img-cart/img3.png"
@@ -7,39 +8,39 @@ import "./Slider.css"
 
 
 
-const product = [
+const productData = [
     {
         id:1 ,
         image:img1 ,
-        prich:"$9.50",
+        price:"$9.50",
         title:"Campos Superior Capsules",
         explanation:"Caramelly & Rich Blend. Sweet like Butterscotch. Flavours like Red Fruit & Apricot.",
     },
     {
         id:2 ,
         image:img2 ,
-        prich:"$9.50",
+        price:"$9.50",
         title:"King St Blend Capsules",
         explanation:"Creamy & Balanced. Sweet like Shortbread. Flavours like Malted Chocolate & Plum",
     },
     {
         id:3 ,
         image:img3 ,
-        prich:"$9.50",
+        price:"$9.50",
         title:"Chapel St Blend Capsules",
         explanation:"Bright & Crisp Blend. Sweet like Red Apple. Flavours like Jasmine.",
     },
     {
         id:4 ,
         image:img4 ,
-        prich:"$9.50",
+        price:"$9.50",
         title:"Decaf Espresso Blend Capsules",
         explanation:"Syrupy & Bold. Sweet like Malt Chocolate. Flavours like Real Coffee.",
     },
     {
         id:5 ,
         image:img5 ,
-        prich:"$9.50",
+        price:"$9.50",
         title:"Campos Capsules Subscription",
         explanation:"Our aluminium capsules are designed to perfectly capture the flavour profile & fresh aroma of our coffees.",
     },
@@ -48,34 +49,30 @@ const product = [
 
 
 const Slider = () => {
+
+
     return(
         <div>
             <div className="div-main">
                 {
-                  product.map((item) => (
+                  productData.map((item , index) => (
                     <div>
-                        <div className="div-cart-main">
-                              <div className="div-img">
+                        <div className="div-cart-main" key={item.id}>
+                              <div className={`div-img div-img-${index + 1}`}>
                                   <div>
                                       <img src={item.image} className="img-cart"/>
                                   </div>
                               </div>
                               <div className="div-main-title">
-                                  <div>
                                       <p className="prich">
-                                        FROM{item.prich}
+                                        FROM{item.price}
                                       </p>
-                                  </div>
-                                  <div>
                                     <h2 className="title-1">
                                       {item.title}
                                     </h2>
-                                  </div>
-                                  <div>
                                     <h5 className="title-2">
                                       {item.explanation}
                                     </h5>
-                                  </div>
                               </div>
                         </div>
                       </div>  

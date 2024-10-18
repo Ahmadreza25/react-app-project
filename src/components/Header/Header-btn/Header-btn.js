c57ffad9e557744e0a2fe91b82f21496c5e55ece
ahmadreza25 <ywsfwnda8@gmail.com>
2024-10-18 15:18:18 +0330

    add code list btn

--- src/components/Header/Header-btn/Header-btn.js
+++ src/components/Header/Header-btn/Header-btn.js
@@ -1,9 +1,15 @@
-import React from "react";
+import React,{useState} from "react";
 import { Button } from "../../styled-components/Button/Button";
+import { ListBtns1 ,ListBtns2, ListBtns3 } from "../../List-btns/ListBtns";
 import icon6 from "../../../images/icon/icon-6.png"
 import "./Header-btn.css"
 
 const HeaderBtn = () => {
+
+    const [butList , setBtnList] = useState(false)
+    const [butList2 , setBtnList2] = useState(false)
+    const [butList3 , setBtnList3] = useState(false)
+
     return(
         <div>
             <div className="div-btn">
@@ -13,19 +19,19 @@ const HeaderBtn = () => {
                     </Button>
                 </div>
                 <div>
-                    <Button size="7px" bag="#fff">
+                    <Button size="7px" bag="#fff" onClick={() => setBtnList((item) => !item)}>
                         <h1>OUR STORY</h1>
                         <img src={icon6} />
                     </Button>
                 </div>
                 <div>
-                    <Button size="7px" bag="#fff">
+                    <Button size="7px" bag="#fff" onClick={() => setBtnList2((item) => !item)}>
                         <h1>SHOP</h1>
                         <img src={icon6} />
                     </Button>
                 </div>
                 <div>
-                    <Button size="7px" bag="#fff">
+                    <Button size="7px" bag="#fff" onClick={() => setBtnList3((item) => !item)}>
                         <h1>SUBSCRIPTIONS</h1>
                         <img src={icon6} />
                     </Button>
@@ -46,6 +52,15 @@ const HeaderBtn = () => {
                     </Button>
                 </div>
             </div>
+            {
+                butList && <ListBtns1 />
+            }
+            {
+                butList2 && <ListBtns2 />
+            }
+            {
+                butList3 && <ListBtns3 />
+            }
         </div>
     )
 }

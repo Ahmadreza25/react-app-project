597bdd84e5580d417122e185d08830c41e33a871
ahmadreza25 <ywsfwnda8@gmail.com>
2024-12-09 15:04:40 +0330

    add to file

---
+++ src/components/ItemsCoffeeTools/ItemCoffeeToolsOne/ItemCoffeeToolsOne.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ItemCoffeeToolsOne.css"
+
+
+
+const ItemCoffeeToolsOne = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ItemCoffeeToolsOne
